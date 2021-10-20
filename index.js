632c1d11d60ba13f31872b6343ec60fe2a925258
JCDC343 <diaz.j@maristcollege.school.nz>
2021-10-20 23:34:58 +1300

    annotated js code, validate submit comment button

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-//Game
+//Game variables and start
 var myGamePiece;
-var myObstacles = [];
+var myObstacles = []; //an array for whenever myObstacles variable is called
 var myScore;
 function startGame() {
     myGamePiece = new component(60, 20, "black", 10, 120);
     myScore = new component("1px", "Courier", "black", 280, 40, "text");
-    myGameArea.start();
+    myGameArea.start(); //starts game
 }
 //Game placement and measurements
 var myGameArea = {
@@ -23,9 +23,9 @@ var myGameArea = {
     }
 }
 
-//Controls game piece movement
-
 //"Template" for myGamePiece and myScore
+
+//myGamePiece function template
 function component(width, height, color, x, y, type) {
     this.type = type;
     this.score = 0;
@@ -35,6 +35,8 @@ function component(width, height, color, x, y, type) {
     this.speedY = 0;    
     this.x = x;
     this.y = y;
+
+//Updates game piece or score accordingly
     this.update = function() {
         ctx = myGameArea.context;
         if (this.type == "text") {
@@ -46,17 +48,23 @@ function component(width, height, color, x, y, type) {
             ctx.fillRect(this.x, this.y, this.width, this.height);
         }
     }
+
+//Function for new position of game piece 
     this.newPos = function() {
         this.x += this.speedX;
         this.y += this.speedY;
         this.hitBottom();
     }
+
+//Function for what to do when game piece hits the bottom
     this.hitBottom = function() {
         var rockbottom = myGameArea.canvas.height - this.height;
         if (this.y > rockbottom) {
             this.y = rockbottom;
         }
     }
+
+//For when game piece hits or doesn't hit an obstacle (otherobj) the code stops or starts again 
     this.crashWith = function(otherobj) {
         var myleft = this.x;
         var myright = this.x + (this.width);
@@ -74,6 +82,7 @@ function component(width, height, color, x, y, type) {
     }
 }
 
+//Changes frames when showing the movement of game piece
 function updateGameArea() {
     var x, height, gap, minHeight, maxHeight, minGap, maxGap;
     for (i = 1; i < myObstacles.length; i += 1) {
@@ -91,8 +100,8 @@ function updateGameArea() {
         minGap = 50;
         maxGap = 200;
         gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
-        myObstacles.push(new component(10, height, "red", x, 0));
-        myObstacles.push(new component(10, x - height - gap, "red", x, height + gap));
+        myObstacles.push(new component(70, height, "red", x, 0));
+        myObstacles.push(new component(70, x - height - gap, "red", x, height + gap));
     }
     for (i = 1; i < myObstacles.length; i += 1) {
         myObstacles[i].x += -5;
@@ -127,4 +136,23 @@ function sFunction() {
     alert ("Thanks for your Comment!");
 return true;
 }
+
+//Validating submit button for comment section
+function validateForm() {
+    let x = document.forms["myForm"]["comment_author"].value;
+    let y = document.forms["myForm"]["comment"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+    if (y == "") {
+        alert("Comment must be filled out");
+        return false;
+      }
+
+return sFunction();
+
+}
+
+
 
